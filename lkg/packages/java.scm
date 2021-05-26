6fa1a21dfc3a179cd9fa05caab16203667038c82
Liāu, Kiong-Gē <gliao.tw@pm.me>
2021-05-26 13:29:01 -0500

    add openjdk16

--- lkg/packages/java.scm
+++ lkg/packages/java.scm
@@ -118,3 +118,49 @@
        ("which" ,which)
        ("zip" ,zip)))
     (home-page "https://openjdk.java.net/projects/jdk/15")))
+
+(define-public openjdk16
+  (package
+    (inherit openjdk15)
+    (name "openjdk")
+    (version "16.0")
+    (source (origin
+              (method url-fetch)
+              (uri "https://github.com/openjdk/jdk/archive/refs/tags/jdk-16-ga.tar.gz")
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0ky99sbrxs6j0la6xwivh5fq04xi0ax3qkzrffs0zmrhghva84b2"))
+              (modules '((guix build utils)))
+              (snippet
+               `(begin
+                  ;; The m4 macro uses 'help' to search for builtins, which is
+                  ;; not available in bash-minimal
+                  (substitute* "make/autoconf/basic.m4"
+                    (("if help") "if command -v"))
+                  (for-each delete-file (find-files "." ".*.(bin|exe|jar)$"))
+                  #t))))
+    (inputs
+     `(("alsa-lib" ,alsa-lib)
+       ("cups" ,cups)
+       ("fontconfig" ,fontconfig)
+       ("freetype" ,freetype)
+       ("giflib" ,giflib)
+       ("lcms" ,lcms)
+       ("libjpeg" ,libjpeg-turbo)
+       ("libpng" ,libpng)
+       ("libx11" ,libx11)
+       ("libxext" ,libxext)
+       ("libxrandr" ,libxrandr)
+       ("libxrender" ,libxrender)
+       ("libxt" ,libxt)
+       ("libxtst" ,libxtst)))
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("make@4.2" ,gnu-make-4.2)
+       ("openjdk15:jdk" ,openjdk15 "jdk")
+       ("pkg-config" ,pkg-config)
+       ("unzip" ,unzip)
+       ("which" ,which)
+       ("zip" ,zip)))
+    (home-page "https://openjdk.java.net/projects/jdk/16")))
